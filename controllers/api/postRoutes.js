227dc789c3efb513b3fa0f144aa2cb77c01a29c7
garrett lieb <liebg08@gmail.com>
2024-02-23 16:29:12 -0800

    fixed routes and models so they work in insomnia for displaying and creating users, posts, and comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,39 +1,67 @@
 const router = require('express').Router();
-// Import the Project model from the models folder
-const { Post } = require('../../models');
+const { Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// If a POST request is made to /api/projects, a new project is created. If there is an error, the function returns with a 400 error. 
-router.post('/', async (req, res) => {
+
+router.get('/', async (req, res) => {
   try {
-    const newPost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newPost);
+      const postData = await Post.findAll({
+          include: [{ model: Comment }],
+      });
+      res.status(200).json(postData);
   } catch (err) {
-    res.status(400).json(err);
+      res.status(500).json(err);
   }
 });
 
-// If a DELETE request is made to /api/projects/:id, that project is deleted. 
+// get one post
+
 router.get('/:id', async (req, res) => {
   try {
-    const postData = await Post.get({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+      const postData = await Post.findByPk(req.params.id, {
+          include: [{ model: Comment }],
+      });
 
-    if (!topicData) {
-      res.status(404).json({ message: 'No Topic found with this id!' });
-      return;
-    }
+      if (!postData) {
+          res.status(404).json({ message: 'No post found with this id!' });
+          return;
+      }
 
-    res.status(200).json(postData);
+      res.status(200).json(postData);
   } catch (err) {
-    res.status(500).json(err);
+      res.status(500).json(err);
+  }
+});
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+      const newPost = await Post.create({
+          ...req.body,
+          user_id: req.session.user_id,
+      });
+      res.status(200).json(newPost);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+      const postData = await Post.destroy({
+          where: {
+              id: req.params.id,
+              user_id: req.session.user_id,
+          },
+      });
+
+      if (!postData) {
+          res.status(404).json({ message: 'No post found with this id!' });
+          return;
+      }
+
+      res.status(200).json(postData);
+  } catch (err) {
+      res.status(500).json(err);
   }
 });
 
