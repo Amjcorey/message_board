df1140a3eeedcab26daba579cd2808ab091fba0f
garrett lieb <liebg08@gmail.com>
2024-02-25 13:05:57 -0800

    sign up link on login page links to signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -24,9 +24,9 @@ const signupFormHandler = async (event) => {
     }
   };
   
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+  // document
+  //   .querySelector('.login-form')
+  //   .addEventListener('submit', loginFormHandler);
   
   document
     .querySelector('.signup-form')
