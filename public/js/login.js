5dc595e638a8eef8c15cf336d1b19448ffff675e
garrett lieb <liebg08@gmail.com>
2024-02-26 15:49:52 -0800

    trying to rever to start where we were a few hours ago...

--- public/js/login.js
+++ public/js/login.js
@@ -1,41 +1,23 @@
 const loginHandler = async (event) => {
-  event.preventDefault();
-
-
-const loginFormHandler = async (event) => {
     event.preventDefault();
-  console.log('im here');
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-  //'checking if email and password are not empty' and if they exist, a POST request is made to /api/users/login with the email and password in the body. 
-  //If the response is ok, the user is redirected to the homepage. If the response is not ok, an alert is shown.
-  console.log(email, password);
-
-
-  if (email && password) {
-      const response = await fetch('/api/users/login', {
-          method: 'POST',
-          body: JSON.stringify({ email: email, password: password }),
-          headers: { 'Content-Type': 'application/json' },
-      });
-
-      if (response.ok) {
-
-        console.log('ok');
-        document.location.replace('/');
-
-
-      } else {
-          alert('Failed to log in');
-      }
-  }
-};
-
-
   
-  document
-    .querySelector('form')
-    .addEventListener('submit', loginFormHandler);
-
+    const email = document.querySelector('#user-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email: email, password: password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to log in');
+        }
+    }
+  };
+  
+  document.querySelector('.login-form').addEventListener('submit', loginHandler);
   
